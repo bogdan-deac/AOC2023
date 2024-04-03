f7bb2d5311f17d5df6c487e1e1ee31e5971eb576
bogdandeac <bogdan.deac@toptal.com>
2024-04-03 19:32:22 +0300

    Day2 & update README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # AOC2023
 
 Advent of code 2023 solved using haskell
+
+## Day1
+
+Easy, just some function composition.
+
+## Day2
+
+Could probably have done it in a cleaner way, but I wanted to use Parsec.
---
+++ day2.hs
@@ -0,0 +1,37 @@
+import Data.Char (isDigit)
+import Control.Arrow ((&&&))
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Char as PC
+import Control.Applicative ((<|>))
+import Data.Functor (($>))
+
+-- solution :: [String] -> Int
+solution = sum . map ((\x -> read x::Int). (\(a,b) -> [a, b]) . (head &&& last))
+
+
+parser :: Parsec.Parsec String () [String]
+parser =  Parsec.many1 ( Parsec.choice
+                        (Parsec.try . Parsec.string <$>
+                            ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight","nine"]
+                        ) <|> PC.letter $> "")
+
+wordToDigit :: String -> String
+wordToDigit "zero" = "0"
+wordToDigit "one" = "1"
+wordToDigit "two" = "2"
+wordToDigit "three" = "3"
+wordToDigit "four" = "4"
+wordToDigit "five" = "5"
+wordToDigit "six" = "6"
+wordToDigit "seven" = "7"
+wordToDigit "eight" = "8"
+wordToDigit "nine" = "9"
+wordToDigit str = str
+
+main = do
+        x <- readFile "day2.txt"
+        let ls = lines x
+        let edigits = [Parsec.parse parser "" l| l <- ls]
+        let digits = [head.wordToDigit <$> filter (/="") a | Right a <- edigits, not (null a)]
+        print $ solution digits
