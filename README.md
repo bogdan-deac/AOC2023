7390bc803518a2010a1ab3921d94f3e76e8d2dde
bogdandeac <bogdan.deac@toptal.com>
2024-04-06 09:58:24 +0300

    Day 2

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Advent of code 2023 solved using haskell
 
 ## Day1
 
+### Part1
+
 Easy, just some function composition.
 
-## Day2
+### Part 2
 
 Could probably have done it in a cleaner way, but I wanted to use Parsec.
+
+## Day 2
+
+Pretty simple, but probably could have done it in a cleaner fashion.
---
+++ day2.hs
@@ -0,0 +1,46 @@
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Char as PC
+import Data.List.Split ( splitOn )
+import Data.Char (isSpace, isDigit)
+
+import Control.Monad (join)
+import qualified Control.Arrow as A ((***), (&&&), Arrow)
+
+
+
+data Game = Game
+  { red :: Int
+  , green :: Int
+  , blue :: Int
+}
+
+gameSplit :: String -> [Game]
+gameSplit input = [ foldl updateGame (Game 0 0 0) (splitOn "," row) | row <- splitOn ";" input]
+
+
+updateGame :: Game -> String -> Game
+updateGame game update = case words update of
+                          [num, field] -> let numI = read num :: Int in case field of
+                               "red" -> game {red = numI}
+                               "green" -> game {green = numI}
+                               "blue" -> game {blue = numI}
+                               _ -> game
+                          _ -> game
+
+isGameValid :: Game -> Bool
+isGameValid game = red game <= 12 && green game <= 13 && blue game <= 14
+
+main :: IO ()
+main = do
+        dataVals <- readFile "data.txt"
+        let lns = drop 5 <$> lines dataVals
+        let indexToGames = [(read (takeWhile isDigit line):: Int, gameSplit $ tail $ dropWhile (not.isSpace) line) |line <- lns]
+        let validGames = filter (\(i, games) -> all isGameValid games) indexToGames
+        print $ sum $ map fst validGames -- part 1
+        let allGames =  snd <$> indexToGames
+        let maxBalls = map (maximum . map red A.&&& maximum. map green A.&&& maximum. map blue) allGames
+        let powers = map (\(a,(b,c)) -> a*b*c) maxBalls
+        print $ sum powers
+
+
+
